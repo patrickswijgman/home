f04151529f4006a83340e0736f22c3418b97c075
Patrick <petrik09@live.nl>
2023-08-07 17:17:27 +0200

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,18 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set theme_powerline_fonts no
+set theme_powerline_fonts yes
 set theme_color_scheme base16-light
 
+abbr --add ga 'git add -u'
+abbr --add gm 'git commit -m'
+abbr --add gch 'git checkout'
+abbr --add gs 'git status'
+abbr --add gdiff 'git diff'
+abbr --add gmer 'git merge --no-ff'
+abbr --add gpull 'git pull --rebase=merges'
+abbr --add gpush 'git push --set-upstream origin HEAD'
+
 alias home='git --git-dir=$HOME/.home.git/ --work-tree=$HOME'
+
+fish_add_path --path /var/lib/flatpak/exports/bin/
