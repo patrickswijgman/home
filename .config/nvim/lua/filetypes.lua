4a437a07890d11d54569899d231f7f3d17467631
Patrick <petrik09@live.nl>
2024-04-04 08:43:45 +0200

    Updates

--- .config/nvim/lua/filetypes.lua
+++ .config/nvim/lua/filetypes.lua
@@ -1,11 +1,16 @@
 -- set filetype for dotenv files, e.g. .env, .env.development, etc
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = ".env*",
-  command = "set filetype=conf"
+  command = "set filetype=config"
 })
 
 -- set filetype for config files
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = { "*/kanshi/config", "*/mako/config", "*/swaylock/config" },
-  command = "set filetype=conf"
+  pattern = { "*/mako/config", "*/swaylock/config" },
+  command = "set filetype=config"
+})
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*/kanshi/config",
+  command = "set filetype=swayconfig"
 })
