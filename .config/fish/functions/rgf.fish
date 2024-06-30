5e587f57e93909d8abc71d63b72313f9b6e49f1c
Patrick <petrik09@live.nl>
2024-06-30 20:31:00 +0200

    Config updates and back to rose-pine colors

--- .config/fish/functions/rgf.fish
+++ .config/fish/functions/rgf.fish
@@ -1,4 +1,5 @@
+# Use the output of rg into fzf.
 function rgf
-    rg --column --color=always --smart-case $argv[1] \
+    rg --column --color=always --smart-case --hidden $argv[1] \
         | fzf --ansi --delimiter=: --bind 'enter:execute:hx {1}:{2}:{3}' --preview 'batcat --style=full --color=always --highlight-line {2} {1}' --preview-window '~4,+{2}+4/2,<80(up)'
 end
