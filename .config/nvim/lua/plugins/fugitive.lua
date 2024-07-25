1cc2f9635854f1d6c3ee6ae1099e593362f0a4b9
Patrick <petrik09@live.nl>
2024-07-25 12:06:03 +0200

    Updated lazy install

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -5,12 +5,5 @@ return {
 
   keys = {
     { "<leader>gs", vim.cmd.Git },
-
-    -- Open merge conflict diff split window for current file
-    -- ]c will go to the next merge conflict
-    -- [c will go to the previous merge conflict
-    { "<leader>gv", function() vim.cmd.Gvdiffsplit { bang = true } end },
-    { "<leader>gu", function() vim.cmd.diffget { args = { "//2" } } end },
-    { "<leader>gh", function() vim.cmd.diffget { args = { "//3" } } end },
   }
 }
