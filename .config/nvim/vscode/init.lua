deb36b9644c73951bcfcb1d97a8290bb11a7a371
patrickswijgman <petrik09@live.nl>
2024-08-27 08:39:07 +0200

    Update init.lua

--- .config/nvim/vscode/init.lua
+++ .config/nvim/vscode/init.lua
@@ -6,13 +6,14 @@ if vim.g.vscode then
     vim.g.mapleader = " "
 
     vim.keymap.set("n", "q", "<Nop>")
-    vim.keymap.set("n", "<leader>r", function() vscode.action("editor.action.rename") end)
     vim.keymap.set("n", "<leader>h", function() vscode.action("workbench.action.focusLeftGroup") end)
     vim.keymap.set("n", "<leader>j", function() vscode.action("workbench.action.focusBelowGroup") end)
     vim.keymap.set("n", "<leader>k", function() vscode.action("workbench.action.focusAboveGroup") end)
     vim.keymap.set("n", "<leader>l", function() vscode.action("workbench.action.focusRightGroup") end)
     vim.keymap.set("n", "<S-h>", function() vscode.action("workbench.action.previousEditorInGroup") end)
     vim.keymap.set("n", "<S-l>", function() vscode.action("workbench.action.nextEditorInGroup") end)
+    vim.keymap.set("n", "cd", function() vscode.action("editor.action.rename") end)
+    vim.keymap.set("n", "ga", function() vscode.action("editor.action.quickFix") end)
     vim.keymap.set("n", "gd", function() vscode.action("editor.action.revealDefinition") end)
     vim.keymap.set("n", "gr", function() vscode.action("editor.action.referenceSearch.trigger") end)
     vim.keymap.set("n", "gt", function() vscode.action("editor.action.goToTypeDefinition") end)
