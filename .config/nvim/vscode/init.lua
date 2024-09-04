7d45bf1860f3ba75cd61280be59334b31afe21d9
patrickswijgman <petrik09@live.nl>
2024-09-04 08:39:38 +0200

    Update init.lua

--- .config/nvim/vscode/init.lua
+++ .config/nvim/vscode/init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -13,30 +15,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     end
 end
 vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = " "
-
-if vim.g.vscode then
-    local vscode = require('vscode')
-
-    vim.notify = vscode.notify
-
-    vim.keymap.set("n", "q", "<Nop>")
-    vim.keymap.set("n", "<Esc>", ":nohl<CR>", { silent = true, remap = true })
-    vim.keymap.set("n", "<leader>h", function() vscode.action("workbench.action.focusLeftGroup") end)
-    vim.keymap.set("n", "<leader>j", function() vscode.action("workbench.action.focusBelowGroup") end)
-    vim.keymap.set("n", "<leader>k", function() vscode.action("workbench.action.focusAboveGroup") end)
-    vim.keymap.set("n", "<leader>l", function() vscode.action("workbench.action.focusRightGroup") end)
-    vim.keymap.set("n", "<S-h>", function() vscode.action("workbench.action.previousEditorInGroup") end)
-    vim.keymap.set("n", "<S-l>", function() vscode.action("workbench.action.nextEditorInGroup") end)
-    vim.keymap.set("n", "cd", function() vscode.action("editor.action.rename") end)
-    vim.keymap.set("n", "ga", function() vscode.action("editor.action.quickFix") end)
-    vim.keymap.set("n", "gd", function() vscode.action("editor.action.revealDefinition") end)
-    vim.keymap.set("n", "gr", function() vscode.action("editor.action.referenceSearch.trigger") end)
-    vim.keymap.set("n", "gt", function() vscode.action("editor.action.goToTypeDefinition") end)
-    vim.keymap.set("n", "]d", function() vscode.action("editor.action.marker.next") end)
-    vim.keymap.set("n", "[d", function() vscode.action("editor.action.marker.prev") end)
-end
 
 require("lazy").setup({
     spec = {
@@ -64,6 +42,36 @@ require("lazy").setup({
         },
         {
             "tpope/vim-surround"
+        },
+        {
+            'vscode-neovim/vscode-multi-cursor.nvim',
+            event = 'VeryLazy',
+            cond = not not vim.g.vscode,
+            config = function()
+                vim.keymap.set('n', '<leader>d', 'mciw*<Cmd>nohl<CR>', { remap = true })
+            end
         }
     },
 })
+
+if vim.g.vscode then
+    local vscode = require('vscode')
+
+    vim.notify = vscode.notify
+
+    vim.keymap.set("n", "q", "<Nop>")
+    vim.keymap.set("n", "<Esc>", ":nohl<CR>", { silent = true, remap = true })
+    vim.keymap.set("n", "<leader>h", function() vscode.action("workbench.action.focusLeftGroup") end)
+    vim.keymap.set("n", "<leader>j", function() vscode.action("workbench.action.focusBelowGroup") end)
+    vim.keymap.set("n", "<leader>k", function() vscode.action("workbench.action.focusAboveGroup") end)
+    vim.keymap.set("n", "<leader>l", function() vscode.action("workbench.action.focusRightGroup") end)
+    vim.keymap.set("n", "<S-h>", function() vscode.action("workbench.action.previousEditorInGroup") end)
+    vim.keymap.set("n", "<S-l>", function() vscode.action("workbench.action.nextEditorInGroup") end)
+    vim.keymap.set("n", "cd", function() vscode.action("editor.action.rename") end)
+    vim.keymap.set("n", "ga", function() vscode.action("editor.action.quickFix") end)
+    vim.keymap.set("n", "gd", function() vscode.action("editor.action.revealDefinition") end)
+    vim.keymap.set("n", "gr", function() vscode.action("editor.action.referenceSearch.trigger") end)
+    vim.keymap.set("n", "gt", function() vscode.action("editor.action.goToTypeDefinition") end)
+    vim.keymap.set("n", "]d", function() vscode.action("editor.action.marker.next") end)
+    vim.keymap.set("n", "[d", function() vscode.action("editor.action.marker.prev") end)
+end
